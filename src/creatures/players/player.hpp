a33a594de1d73a34c5f749d7772d8198477c923c
João Paulo <jprzimba@gmail.com>
2024-04-20 12:42:52 -0300

    Fix check nullptr town (avoid crash if town not exist)

--- src/creatures/players/player.hpp
+++ src/creatures/players/player.hpp
@@ -665,6 +665,10 @@ public:
 		return loginPosition;
 	}
 	const Position &getTemplePosition() const {
+		if (!town) {
+			static auto emptyPosition = Position();
+			return emptyPosition;
+		}
 		return town->getTemplePosition();
 	}
 	std::shared_ptr<Town> getTown() const {
