34a6f88ae358558a9e6b155207ba1645600e65cf
João Paulo <jprzimba@gmail.com>
2024-04-27 18:38:07 -0300

    [FIX] Utani Gran Hur (Strong Haste) duration
    https://tibia.fandom.com/wiki/Strong_Haste

--- data/scripts/spells/support/strong_haste.lua
+++ data/scripts/spells/support/strong_haste.lua
@@ -3,7 +3,7 @@ combat:setParameter(COMBAT_PARAM_EFFECT, CONST_ME_MAGIC_GREEN)
 combat:setParameter(COMBAT_PARAM_AGGRESSIVE, 0)
 
 local condition = Condition(CONDITION_HASTE)
-condition:setParameter(CONDITION_PARAM_TICKS, 22000)
+condition:setParameter(CONDITION_PARAM_TICKS, 21000)
 condition:setFormula(1.7, 40, 1.7, 40)
 combat:addCondition(condition)
 
