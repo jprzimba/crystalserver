9b16328ac55f05733380aa6e87c2440024342d93
João Paulo <jprzimba@gmail.com>
2024-05-12 15:49:58 -0300

    Fix possible crash in creatureevents

--- src/lua/creature/creatureevent.cpp
+++ src/lua/creature/creatureevent.cpp
@@ -113,11 +113,9 @@ CreatureEvent::CreatureEvent(LuaScriptInterface* interface) :
 	Script(interface) { }
 
 void CreatureEvents::removeInvalidEvents() {
-	for (auto it = creatureEvents.begin(); it != creatureEvents.end(); ++it) {
-		if (it->second->getScriptId() == 0) {
-			creatureEvents.erase(it->second->getName());
-		}
-	}
+	std::erase_if(creatureEvents, [](const auto &pair) {
+		return pair.second->getScriptId() == 0;
+	});
 }
 
 std::string CreatureEvent::getScriptTypeName() const {
