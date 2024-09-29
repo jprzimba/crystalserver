5053538b3bee5a4236f309892c636f0478b54f67
João Paulo <jprzimba@gmail.com>
2024-09-29 10:52:05 -0300

    * Fix NPCs purchase items without capacity check

--- src/game/game.cpp
+++ src/game/game.cpp
@@ -2370,7 +2370,7 @@ std::tuple<ReturnValue, uint32_t, uint32_t> Game::addItemBatch(const std::shared
 				if (item->getContainer()) {
 					containersCreated++;
 				}
-				totalAdded++;
+				totalAdded += item->getItemCount();
 			}
 
 			ret = returnError;
